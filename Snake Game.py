3aaac88ef9ef85d2ba675b434b791a4218384753
maxcarvajalhu <132011927+maxcarvajalhu@users.noreply.github.com>
2023-05-03 17:27:48 -0600

    Instalación de librerias y def create_board
    Se actualizó el archivo agregando las librerias numpy, pygame, sys y math. Se iniciaron las variables Blue, Black, Red. Yellow, Row Count y Column Count. Además se agregó la función create board en la que se crea la estructura del tablero de juego de connect.

---
+++ Snake Game.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pygame
+import sys
+import math
+
+BLUE = (0,0,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+YELLOW = (255,255,0)
+
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+
+def create_board():
+	board = np.zeros((ROW_COUNT,COLUMN_COUNT))
+	return board
