85dc988b1f06009e34c9ebc63858db2434764809
maxcarvajalhu <132011927+maxcarvajalhu@users.noreply.github.com>
2023-05-09 10:46:59 -0600

    Update Snake Game.py

--- Snake Game.py
+++ Snake Game.py
@@ -14,6 +14,16 @@ COLUMN_COUNT = 7
 def create_board():
 	board = np.zeros((ROW_COUNT,COLUMN_COUNT))
 	return board
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+ 
+def is_valid_location(board, col):
+    return board[ROW_COUNT-1][col] == 0
+ 
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
 
 def print_board(board):
     print(np.flip(board, 0))
